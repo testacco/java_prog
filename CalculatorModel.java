2443c4b3574f5c6a6c2986604bba6f3ac6fff6de
Mariia <mt@mail.com>
2018-03-25 16:55:42 +0000

    initial commit

---
+++ CalculatorModel.java
@@ -0,0 +1,14 @@
+package calculator;
+
+class CalculatorModel {
+
+    private int calculationValue;
+    public void addTwoNumbers(int firstNumber, int secondNumber){
+      calculationValue = firstNumber + secondNumber;
+    }
+
+    public int getCalculationValue(){
+      return calculationValue;   
+    }    
+    
+}
